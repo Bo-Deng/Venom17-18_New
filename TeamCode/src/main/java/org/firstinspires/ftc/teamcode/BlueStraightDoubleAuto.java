40df231fb28cbce9f90628bf6594f61cd7841374
Hamza A <hamzaimran2011@gmail.com>
2018-02-05 16:42:25 -0600

    Straight Auto Code added
    You'll probably have to check this, as I doubt its correct. Especially the moveSquares and Pturn parameters.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueStraightDoubleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueStraightDoubleAuto.java
@@ -35,7 +35,7 @@ public class BlueStraightDoubleAuto extends CustomLinearOpMode {
         DebugLog.LOGE("startDistance ", "" + getLeftDistance());
 
 
-        
+
         if (template == 'L') {
             //strafe left
             strafeBlueAssistedPID(47.0, 180);
@@ -62,5 +62,47 @@ public class BlueStraightDoubleAuto extends CustomLinearOpMode {
         backUp();
 
         //second block
+        Pturn(90);
+        Pturn(45);
+        Thread.sleep(250);
+
+        moveSquares(1.45,1);
+        Thread.sleep(250);
+
+        grabBlock(); //we'll want to make sure this method actually lifts the block high enough to stack.
+        Pturn(-90);
+        Pturn(-90);
+
+        moveSquares(1.45, 1);
+        Thread.sleep(250);
+
+        Pturn(45);
+        stopMotors();
+        Thread.sleep(500);
+
+        if (template == 'L') {
+            //strafe left
+            strafeBlueAssistedPID(47.0, 180);
+
+        } else if (template == 'C') {
+            // align with center column
+            strafeBlueAssistedPID(63.7, 180);
+
+        } else if (template == 'R') {
+            //strafe right
+            //strafeBlueAssisted(.5, 77, 180);
+            strafeBlueAssistedPID(81.7, 180);
+        } stopMotors();
+
+        liftDown();
+        Thread.sleep(500);
+
+        servoLHug.setPosition(.4);
+        servoRHug.setPosition(.6);
+
+        wiggle(.4, 180);
+        stopMotors();
+        sleep(250);
+        backUp();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedStraightDoubleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedStraightDoubleAuto.java
@@ -54,5 +54,46 @@ public class RedStraightDoubleAuto extends CustomLinearOpMode {
         backUp();
 
         //second block
+        Pturn(-90);
+        Pturn(-45);
+        Thread.sleep(250);
+
+        moveSquares(1.45,1);
+        Thread.sleep(250);
+
+        grabBlock(); //we'll want to make sure this method actually lifts the block high enough to stack.
+        Pturn(90);
+        Pturn(90);
+
+        moveSquares(1.45, 1);
+        Thread.sleep(250);
+
+        Pturn(-45);
+        stopMotors();
+        Thread.sleep(500);
+
+        if (template == 'L') {
+            //strafe left
+            strafeRedAssistedPID(80.4, 0);
+
+        } else if (template == 'C') {
+            // align with center column
+            strafeRedAssistedPID(62.9, 0);
+
+        } else if (template == 'R') {
+            //strafe right
+            strafeRedAssistedPID(46, 0);
+        } stopMotors();
+
+        liftDown();
+        Thread.sleep(500);
+
+        servoLHug.setPosition(.4);
+        servoRHug.setPosition(.6);
+
+        wiggle(.4, 0);
+        stopMotors();
+        sleep(250);
+        backUp();
     }
 }
